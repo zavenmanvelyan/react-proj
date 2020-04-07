805f1359b17ff2ec31d54a0183548ec492016cf3
zavenmanvelyan <zaven.manvelyan.y@tumo.org>
2020-04-07 10:33:05 +0400

    Updates

---
+++ precache-manifest.50e2fef4098706d7089c161e550c0280.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1ecdecaad516c32dafa467c56ce42b8",
+    "url": "/react-proj/index.html"
+  },
+  {
+    "revision": "ba83917b1d8e564941c2",
+    "url": "/react-proj/static/css/3.193b8085.chunk.css"
+  },
+  {
+    "revision": "4dde7be0770164970024",
+    "url": "/react-proj/static/css/4.338b7556.chunk.css"
+  },
+  {
+    "revision": "49956c2001801ed17d9d",
+    "url": "/react-proj/static/css/main.41de099f.chunk.css"
+  },
+  {
+    "revision": "4eb5cfd3536852fa4bf3",
+    "url": "/react-proj/static/js/2.ff3be1aa.chunk.js"
+  },
+  {
+    "revision": "ba83917b1d8e564941c2",
+    "url": "/react-proj/static/js/3.49e786ab.chunk.js"
+  },
+  {
+    "revision": "4dde7be0770164970024",
+    "url": "/react-proj/static/js/4.cbeb1dae.chunk.js"
+  },
+  {
+    "revision": "49956c2001801ed17d9d",
+    "url": "/react-proj/static/js/main.0310ee12.chunk.js"
+  },
+  {
+    "revision": "977075523633f28f46ed",
+    "url": "/react-proj/static/js/runtime~main.ae4bc9f3.js"
+  },
+  {
+    "revision": "9064981b80e59f1b663c17fab41563c9",
+    "url": "/react-proj/static/media/Spinner-1s-200px.9064981b.svg"
+  },
+  {
+    "revision": "d451ffa50f922985949399f15b979919",
+    "url": "/react-proj/static/media/user.d451ffa5.jpg"
+  }
+]);
